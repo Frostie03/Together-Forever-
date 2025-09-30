be2f0691120b4f61411a7ea0e66fbaa06e9b0128
Nikki <nikkitag03@gmail.com>
2025-10-01 01:47:03 +0530

    uploaded files

---
+++ Katendi/script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'none'
+}
+function KN(){
+  alert("My love for you is as boundless as the sky, stretching beyond the horizon, and I’ll cherish you always, holding you close in my heart no matter where life takes us.");
+}
