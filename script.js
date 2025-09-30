fbb5f93c850a66f227842f12ff7f0cab7d1dbe51
Nikki <142841528+Frostie03@users.noreply.github.com>
2025-10-01 01:33:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'none'
+}
+function KN(){
+  alert("My dearest love, Your love is the compass guiding every step of my heart, my heart is yours, bound by an unbreakable bond that no distance or force could ever shatter. I vow to stand by your side, cherishing and adoring you with every beat of my heart, now and forever.");
+}
