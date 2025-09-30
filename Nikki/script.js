8eff0edc277f34355118b250e9ae43d0263106d5
Nikki <142841528+Frostie03@users.noreply.github.com>
2025-10-01 01:38:52 +0530

    Add files via upload

---
+++ Nikki/script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'none'
+}
+function KN(){
+  alert("My darling, my heart is yours forever; no distance can dim my love, and I’ll cherish you always.");
+}
